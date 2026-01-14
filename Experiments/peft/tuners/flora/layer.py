8c5b35299442fc8598e2647b302686491f144d99
Hao Chen <ch981129@126.com>
2026-01-14 14:12:29 +0000

    Update layer.py

--- Experiments/peft/tuners/flora/layer.py
+++ Experiments/peft/tuners/flora/layer.py
@@ -11,6 +11,16 @@ from .config import FloraConfig
 from .gates import Gate
 
 
+from copy import deepcopy
+
+import torch
+import torch.nn.functional as F
+from torch import nn
+
+from peft.utils.integrations import dequantize_module_weight, gather_params_ctx
+from peft.utils.other import transpose
+
+
 def _to_hwc(z: torch.Tensor) -> Tuple[torch.Tensor, Tuple[int, int, int], int]:
     orig_ndim = z.ndim
     if z.ndim >= 4:
@@ -101,7 +111,8 @@ class FloraLinear(nn.Module):
         B = nn.Linear(r, self.out_features, bias=False)
 
         # Original LoRA init style
-        nn.init.kaiming_uniform_(A.weight, a=5**0.5, nonlinearity='leaky_relu')
+        # nn.init.kaiming_uniform_(A.weight, a=5**0.5, nonlinearity='leaky_relu')
+        nn.init.xavier_uniform_(A.weight)
         nn.init.zeros_(B.weight)
 
         # --- register under PEFT-recognized names ---
@@ -121,7 +132,8 @@ class FloraLinear(nn.Module):
         # scaling
         # This is for LoRA
         # self.scaling[adapter_name] = float(cfg.lora_alpha) / float(r)
-        flora_nonlinear_scale = 0.5
+
+        flora_nonlinear_scale = 1.0
         self.scaling[adapter_name] = float(cfg.lora_alpha) / float(r) * flora_nonlinear_scale
 
         self.norm_before_act[adapter_name] = nn.LayerNorm(r)
@@ -208,12 +220,14 @@ class FloraLinear(nn.Module):
         gateB = self.gate_after_b[name]
         scale = self.scaling[name]
 
-        norm  = self.norm_before_act.get(name, None)
+        norm  = self.norm_before_act[name]
 
-        z = A(drop(x))
+        # z = A(drop(x))
 
-        if norm is not None and not isinstance(norm, nn.Identity):
-            z = norm(z)
+        z = A(x)  #
+
+        # if norm is not None and not isinstance(norm, nn.Identity):
+        #     z = norm(z)
 
         z = gateA(z)
 
@@ -222,10 +236,118 @@ class FloraLinear(nn.Module):
             z_hwc = act(z_hwc)
             z = _from_hwc(z_hwc, orig_ndim)
 
+            z = z.clamp(-10.0, 10.0)
+
+
         dz = B(z)
         dz = gateB(dz)
 
-        if hasattr(self, 'magnitude') and name in self.magnitude:
-            dz = self.magnitude[name] * F.normalize(dz, dim=-1)
+        # if hasattr(self, 'magnitude') and name in self.magnitude:
+        #     dz_norm = dz.norm(p=2, dim=-1, keepdim=True).detach() + 1e-8
+        #     dz = self.magnitude[name] * (dz / dz_norm)
+
+        dz = self.magnitude[name] * dz
 
         return y + dz * scale
+
+
+
+
+# Copyright 2024-present the HuggingFace Inc. team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+
+class DoraLinearLayer(nn.Module):
+    def __init__(self, fan_in_fan_out):
+        super().__init__()
+        self.fan_in_fan_out = fan_in_fan_out
+
+    def get_weight_norm(self, weight, lora_weight, scaling) -> torch.Tensor:
+        # calculate L2 norm of weight matrix, column-wise
+        weight = transpose(weight, self.fan_in_fan_out)
+        weight = weight + scaling * lora_weight
+        weight_norm = torch.linalg.norm(weight, dim=1).to(weight.dtype)
+        return weight_norm
+
+    def update_layer(self, *, base_layer, lora_A, lora_B, scaling, place_on_cpu=False) -> None:
+        # temporarily convert fp16 to fp32, as fp16 can cause trouble on CPU with PyTorch < 2.2
+        dtype_is_fp16 = lora_A.dtype == torch.float16
+        if dtype_is_fp16:
+            lora_A = lora_A.float()
+            lora_B = lora_B.float()
+
+        with gather_params_ctx(base_layer.parameters()):
+            if base_layer.__class__.__name__ == "Linear4bit":
+                # We have to create a copy of the base layer, otherwise, FSDP will throw an error. 8bit does not work
+                # yet because Int8Params cannot be correctly deep-copied (attributes vanish)
+                base_layer = deepcopy(base_layer)
+
+            weight = dequantize_module_weight(base_layer)
+            if weight.data.ndim >= 3:  # For handling LoRAs applied to Conv layers.
+                r = lora_A.shape[0]
+                lora_weight = torch.mm(lora_B.view([-1, r]), lora_A.view([r, -1]))
+                lora_weight = lora_weight.reshape(weight.shape)
+            else:
+                lora_weight = lora_B @ lora_A
+
+            if dtype_is_fp16:
+                lora_weight = lora_weight.half()
+            weight_norm = self.get_weight_norm(weight.to(lora_A.device), lora_weight, scaling)
+
+        if place_on_cpu:
+            weight_norm = weight_norm.to("cpu")
+        self.weight = nn.Parameter(weight_norm, requires_grad=True)
+
+    def forward(self, x, *, lora_A, lora_B, scaling, base_layer, base_result=None):
+        """
+        For DoRA, calculate the extra output from LoRA with DoRA applied. This should be added on top of the base layer
+        output.
+        """
+        # Don't use `lora_weight = lora_B.weight @ lora_A.weight` because this causes errors with FSDP. Instead,
+        # calculate the same but using forward.
+        x_eye = torch.eye(lora_A.weight.shape[1], device=lora_A.weight.device, dtype=x.dtype)
+        lora_weight = lora_B(lora_A(x_eye)).T
+
+        magnitude = self.weight
+        weight = dequantize_module_weight(base_layer)
+        weight = weight.to(x.dtype)
+        weight_norm = self.get_weight_norm(weight, lora_weight.detach(), scaling)
+        # see section 4.3 of DoRA (https://huggingface.co/papers/2402.09353)
+        # "[...] we suggest treating ||V +∆V ||_c in
+        # Eq. (5) as a constant, thereby detaching it from the gradient
+        # graph. This means that while ||V + ∆V ||_c dynamically
+        # reflects the updates of ∆V , it won’t receive any gradient
+        # during backpropagation"
+        weight_norm = weight_norm.detach()
+        mag_norm_scale = (magnitude / weight_norm).view(1, -1)
+
+        lora_result = lora_B(lora_A(x))
+
+        bias = None
+        if base_result is not None:
+            bias = base_layer.bias
+            if bias is not None:
+                base_result = base_result - bias
+        else:
+            base_result = F.linear(x, transpose(weight, self.fan_in_fan_out))
+
+        result_dora = (mag_norm_scale - 1) * base_result + mag_norm_scale * lora_result * scaling
+
+        return result_dora
+
+    def __repr__(self) -> str:
+        rep = super().__repr__()
+        return "lora.dora." + rep
+
